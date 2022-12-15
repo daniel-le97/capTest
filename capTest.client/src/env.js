5b5d34ef422f0a4908b4dd71a87abfb046155982
daniel <dnaiel97.le@gmail.com>
2022-12-15 13:33:50 -0700

    test

--- capTest.client/src/env.js
+++ capTest.client/src/env.js
@@ -1,6 +1,7 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const dev = window.location.origin.includes("localhost");
+export const baseURL = dev ? "http://localhost:3000" : "";
+export const useSockets = false;
+export const domain = "bobocat.us.auth0.com";
+export const audience = "https://bcw-demos.com";
+export const clientId = "Acd91zZtHgbEyChg4MW53440xBdm6cqE";
+
